07c04f917c7d29568d48252835f4600948168022
rodrwankenobi <49798159+rodrwankenobi@users.noreply.github.com>
2021-02-21 16:36:14 -0300

    First commit

---
+++ batch/Producer_customer_topic.bat
@@ -0,0 +1,1 @@
+start C:\Kafka\application\bin\windows\kafka-console-producer.bat --topic customertopic --broker-list localhost:9092,localhost:9093 
---
+++ batch/Producer_product_topic.bat
@@ -0,0 +1,1 @@
+start C:\Kafka\application\bin\windows\kafka-console-producer.bat --topic producttopic --broker-list localhost:9092,localhost:9093 
